aac65d60e985fbf42fcc44993634b9e2dd8d0cda
syauqeesy <syauqeesy@gmail.com>
2020-10-28 08:57:59 +0700

    Add 22-10-2020 tasks

---
+++ 22-10-2020/1.js
@@ -0,0 +1,41 @@
+const biodata = {
+  name: `Ahmad Syauqi`,
+  age: 18,
+  hobbies: [ `Listening Music`, `Programming`, `Technology` ],
+  isMarried: false,
+  schoolList: [
+    {
+      name: `SDN Dadali`,
+      yearIn: 2009,
+      yearOut: 2014,
+      major: null
+    },
+    {
+      name: `SMPN 41 Bandung`,
+      yearIn: 2014,
+      yearOut: 2017,
+      major: null
+    },
+    {
+      name: `SMKN 11 Bandung`,
+      yearIn: 2017,
+      yearOut: 2020,
+      major: `Rekayasa Perangkat Lunak`
+    }
+  ],
+  skills: [
+    {
+      skillName: `HTML`,
+      level: `advanced`
+    },
+    {
+      skillName: `CSS`,
+      level: `advanced`
+    },
+    {
+      skillName: `JS`,
+      level: `advanced`
+    }
+  ],
+  interestInCoding: true
+}
