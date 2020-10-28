73d9272e1c4592fbda2da734736a67e33adbdc83
syauqeesy <syauqeesy@gmail.com>
2020-10-28 16:18:37 +0700

    Task 27-10-2020 number 3 finished

---
+++ 27-10-2020/3.js
@@ -0,0 +1,58 @@
+const strongPasswordGenerator = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let generatedPassword = ''
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-='
+      for (let i = 0; i < 16; i++) {
+        generatedPassword += characters.charAt(Math.floor(Math.random() * characters.length))
+      }
+      
+      if(generatedPassword) {
+        resolve(generatedPassword)
+      } else {
+        reject('Generate password failed')
+      }
+    }, 1000)
+  })
+}
+
+strongPasswordGenerator()
+  .then((generatedPassword) => {
+    console.log(generatedPassword)
+  })
+  .catch((error) => {
+    console.log(error.message)
+  })
+
+
+const getProductById = (id) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const products = [
+        { id: 1, name: 'Laptop Gaming', desc: 'Super powerfull gaming laptop' },
+        { id: 2, name: 'Samsung Smartphone', desc: 'Samsung smartphone for everyone' },
+        { id: 3, name: 'Logitech Office Mouse', desc: 'Basic Mouse' },
+        { id: 1, name: 'Logitech G Ambidextrous', desc: 'Gaming mouse for prodigy' },
+        { id: 1, name: 'Audio-Technica Monitor Headphone', desc: 'Headphone for professional producer' }
+      ]
+
+      const product = products.filter((product) => {
+        return product.id === id
+      })
+
+      if(!product.length < 1) {
+        resolve(product)
+      } else {
+        reject(new Error('Product not found'))
+      }
+    }, 1000)
+  })
+}
+
+getProductById(3)
+  .then((product) => {
+    console.log(product)
+  })
+  .catch((error) => {
+    console.log(error.message)
+  })
